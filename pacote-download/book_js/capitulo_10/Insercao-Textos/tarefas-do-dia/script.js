f762734800f522d23c6d2d9773f31a6510f05257
Jose <josecarlos099916@gmail.com>
2024-01-20 21:31:44 -0300

    Criei os botões selecionar e remover tarefa

--- pacote-download/book_js/capitulo_10/Insercao-Textos/tarefas-do-dia/script.js
+++ pacote-download/book_js/capitulo_10/Insercao-Textos/tarefas-do-dia/script.js
@@ -16,3 +16,55 @@ frm.addEventListener('submit', (e) => {
     frm.inTarefa.focus()
 })
 
+frm.btSelecionar.addEventListener('click', () => {
+
+    const tarefas = document.querySelectorAll('h5')
+
+    if (tarefas.length == '') {
+        alert('Não há tarefas para selecionar')
+        return
+    }
+
+    let aux = -1 // VARIÁVEL AUXILIAR PARA INDICAR LINHA SELECIONADA
+
+    // PERCORRE A LISTA DE ELEMENTOS h5 INSERIDOS NA PÁGINA, OU SEJA, TAREFAS
+    for (let i = 0; i < tarefas.length; i++) {
+        // SE TAG É DA CLASS tarefa-selecionada (ESTÁ SELECIONADA)
+        if (tarefas[i].className == 'tarefa-selecionada') {
+            tarefas[i].className = 'tarefa-normal'
+            aux = i
+            break
+        }
+    }
+
+    // SE A LINHA Q ESTÁ SELECIONADA É A ÚLTIMA, IRÁ VOLTAR PARA A 1º
+    if (aux == tarefas.length - 1) {
+        aux = -1
+    }
+
+    // MUDA ESTILO DA PRÓXIMA LINHA
+    tarefas[aux + 1].className = 'tarefa-selecionada'
+})
+
+frm.btRetirar.addEventListener('click', () => {
+    const tarefas = document.querySelectorAll('h5')
+
+    let aux = -1
+
+    tarefas.forEach((tarefa, i) => {
+        if (tarefa.className == 'tarefa-selecionada') {
+            aux = i
+        }
+    })
+
+    if (aux == -1) {
+        alert('Selecione uma tarefa para removê-la')
+        return
+    }
+
+    // SOLICITA CONFIRMAÇÃO (EXIBINDO O CONTEÚDO DA TAG h5 SELECIONADA)
+    if (confirm(`Confirma exclusão de "${tarefas[aux].innerText}"?`)) {
+        dvQuadro.removeChild(tarefas[aux]) // TEM QUE REFERENCIAR A TAG PAI, NO CASO dvQuadro
+    }
+})
+
