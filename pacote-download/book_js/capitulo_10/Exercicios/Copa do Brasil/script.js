cae3f3c6f150f696a0b361131510a843745e44df
Jose <josecarlos099916@gmail.com>
2024-02-07 23:24:23 -0300

    Criei a estrutura, estilo e iniciei as funções

---
+++ pacote-download/book_js/capitulo_10/Exercicios/Copa do Brasil/script.js
@@ -0,0 +1,13 @@
+const frm = document.querySelector('.form')
+const addClube = document.getElementById('addClube')
+
+frm.addEventListener('submit', (event) => {
+    event.preventDefault()
+
+
+})
+
+addClube.addEventListener('click', () => {
+    alert('ok')
+})
+
